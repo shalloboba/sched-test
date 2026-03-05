2841844a62f79a0257f0310a50c4015c05819c8f
shallotte_boba_ <shimarinosI@gmail.com>
2026-03-05 22:16:50 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,21 @@
-const CACHE="sched-v1"
+const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js"
+];
 
-const ASSETS=[
-"/",
-"/index.html"
-]
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
 
-self.addEventListener("install",e=>{
-e.waitUntil(
-caches.open(CACHE).then(cache=>{
-return cache.addAll(ASSETS)
-})
-)
-})
-
-self.addEventListener("fetch",e=>{
-e.respondWith(
-caches.match(e.request).then(res=>{
-return res || fetch(e.request)
-})
-)
-})
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
